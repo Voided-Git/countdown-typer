18c4dc4257f4ed19dd6079ba65720f8cdae9d3e3
RRyan2447 <ryanjohnsjuan@outlook.com>
2021-11-21 18:09:45 +0000

    Upload files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# countdown-typer
- 
+# Countdown Typer #
+Has your friend ever messaged you saying something like "5 mins" or "10 minutes", responding by counting them down through messages until the timer reaches 0 at which point you spam them to finish what they were doing? This application is just for you, simply run, enter the amount of time to countdown (shorthand times are supported) and press 't' to type when your on your messaging platform.
+
+### Supported Shorthands ###
+In CountdownTyper you can use a handful of shorthand times for making it user friendly. The full list of currently supported shorthands is as follows:
+- 0 → raw seconds
+- 0`s` → seconds
+- 0`m` → minutes
+- 0`h` → hours
+- 0`d` → days
+- 0`w` → weeks
+- 0`mo` → months
+- 0`y` → years
+- 0`de` → decades
+- 0`c` → centuries
+- 0`mi` → millennia
+- `inf` → counts down from infinity (yes really)
---
+++ countdown-typer.py
@@ -0,0 +1,112 @@
+# keyboard module
+from keyboard import write, wait, press_and_release
+from os import system
+from time import sleep
+
+
+# set the window title
+system("title CountdownTyper")
+
+
+# check if an input is integer
+def is_int(args: str):
+    try:
+        int(args)
+        return True
+    except ValueError:
+        return False
+
+
+# time inputting
+def time_input(prompt: str):
+    time = "NONE"
+    while not is_int(time) and not is_int(time[:-1]) and not is_int(time[:-2]) and not time.upper() == "INF":
+        time = input(prompt)
+
+    return time
+
+
+# convert time
+def raw_time(time: str):
+    _1end = time[len(time) - 1].upper()
+    _2end = time[len(time) - 2].upper()
+    _1rawtime = int(time[:-1])
+    if not is_int(_1rawtime):
+        _2rawtime = int(time[:-2])
+
+    if is_int(time) or _1end == "S":
+        return _1rawtime
+
+    elif _1end == "M":
+        return _1rawtime * 60
+
+    elif _1end == "H":
+        return _1rawtime * 3600
+
+    elif _1end == "D":
+        return _1rawtime * 86400
+
+    elif _1end == "W":
+        return _1rawtime * 604800
+
+    elif _2end == "MO":
+        return _2rawtime * 2592000
+
+    elif _1end == "Y":
+        return _1rawtime * 31536000
+
+    elif _2end == "DE":
+        return _2rawtime * 315360000
+
+    elif _1end == "C":
+        return _1rawtime * 3153600000
+
+    elif _2end == "MI":
+        return _2rawtime * 31536000000
+
+    elif time.upper() == "INF":
+        return "INFINITY"
+
+    return False
+
+
+# infinite sender
+def infinity_sender():
+    i = 0
+    write("∞")
+
+    while True:
+        i += 1
+        write(f"∞ - {i}")
+        press_and_release("return")
+        sleep(1)
+
+
+# main loop
+while True:
+    # asking for the time sent
+    time = time_input("Enter the time sent: ")
+    time = raw_time(time)
+
+    # logical processing
+    if time:
+        # waiting for 't' and removing the 't' once pressed
+        print("Press 't' to start counting down.")
+        wait("t")
+        press_and_release("backspace")
+
+        if time == "INFINITY":
+            # sending from infinity
+            infinity_sender()
+
+        else:
+            # sending the message x number of times subtracting 1 each time
+            for i in range(0, time + 1):
+                write(str(time - i))
+                press_and_release("return")
+                sleep(1)
+
+    # when finished
+    input("All done! (press enter to continue)")
+    # cleanup
+    system("cls")
